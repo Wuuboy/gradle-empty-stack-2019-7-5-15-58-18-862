d9b35f4cc9e8e8d91e6e6ceaa6dd41bf20f21f80
曹桂凤 <demi.cao@oocl.com>
2019-07-11 14:41:04 +0800

    feat:implement story 1.1

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println(new App().getGreeting());
+        Mobile mobile = new Mobile("huawei mate20","blue","huawei");
+         mobile.call();
+    }
+}
---
+++ src/main/java/Mobile.java
@@ -0,0 +1,15 @@
+public class Mobile {
+    private String name;
+    private String color;
+    private String brand;
+
+    public Mobile(String name, String color, String brand) {
+        this.name = name;
+        this.color = color;
+        this.brand = brand;
+    }
+
+    public void call(){
+        System.out.println("Message : "+ name +":"+ color +":"+ brand);
+    }
+}
